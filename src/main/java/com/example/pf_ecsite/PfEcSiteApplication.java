cb5ac24a6cef3ffd3700c969f0432e3ee67a63c9
shofas0921 <motohashi.sho@gmail.com>
2025-03-21 14:28:58 +0900

    create project

---
+++ src/main/java/com/example/pf_ecsite/PfEcSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.pf_ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfEcSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfEcSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pf_ecsite/PfEcSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pf_ecsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfEcSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
